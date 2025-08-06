a0d20dcf6a62b1f69ce8246a226ab5875b87ce5c
Atul-Kumar-Ranjan <atul@noesyssoftware.com>
2025-08-06 11:33:43 +0530

    tags

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import type { FC } from "react";
 import React from "react";
 import ButtonPrimary from "@/shared/Button/ButtonPrimary";
-import { Product } from "@/store/product-store";
+import { numberFormatter, Product } from "@/store/product-store";
 import BrandButton from "@/shared/Button/BrandButton";
 
 interface ProductCardProps extends Product {
@@ -16,6 +16,7 @@ const ProductCard: FC<ProductCardProps> = ({
   name,
   price,
   id,
+  soh,
   category
 }) => {
   return (
@@ -25,11 +26,6 @@ const ProductCard: FC<ProductCardProps> = ({
     >
       <div className="">
         <div className="relative overflow-hidden">
-          {/* {onSale && (
-            <span className="absolute left-2 top-2 z-10 rounded-sm bg-green-700 px-2 text-sm font-bold text-white">
-              Sale
-            </span>
-          )} */}
           <div className="relative aspect-square bg-white">
             <Image
               src={image}
@@ -38,6 +34,20 @@ const ProductCard: FC<ProductCardProps> = ({
               className="object-contain"
               sizes="100%"
             />
+            {/* Stock status badges */}
+            {soh === -1 ? (
+              <span className="absolute right-4 top-4 bg-red-500 px-2 py-1 text-xs font-bold text-white shadow-md">
+                OUT OF STOCK
+              </span>
+            ) : soh === 0 ? (
+              <span className="absolute right-4 top-4 bg-yellow-500 px-2 py-1 text-xs font-bold text-white shadow-md">
+                BACK ORDER
+              </span>
+            ) : (
+              <span className="absolute right-4 top-4 bg-green-500 px-2 py-1 text-xs font-bold text-white shadow-md">
+                IN STOCK : {numberFormatter(soh)}
+              </span>
+            )}
           </div>
           <div className="absolute bottom-2 w-full px-4 text-center transition-all duration-300 group-hover:bottom-2 lg:-bottom-full">
             <ButtonPrimary className="w-full text-sm">Quick View</ButtonPrimary>
@@ -46,18 +56,7 @@ const ProductCard: FC<ProductCardProps> = ({
         <div className="px-5 py-4">
           <span className="text-xs bg-blue-200 px-2 rounded-md">{category}</span>
           <h3 className="line-clamp-2 text-ellipsis font-bold min-h-12">{name}</h3>
-          {/* {onSale ? (
-            <p>
-              <span className="font-bold text-green-700">
-                ${price}.00
-              </span>{" "}
-              <span className="text-sm font-semibold text-neutral-500 line-through">
-                ${price}.00
-              </span>
-            </p>
-          ) : ( */}
             <p className="font-bold text-blue-700">${price}</p>
-          {/* )} */}
         </div>
         <div className="w-full px-4 pb-4 text-center">
             <BrandButton className="w-full">Add to Cart</BrandButton>
