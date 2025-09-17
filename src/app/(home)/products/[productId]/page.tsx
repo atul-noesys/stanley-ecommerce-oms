2b2366a3e502c947de123d8f0a863bd66a04db15
Atul-Kumar-Ranjan <atul@noesyssoftware.com>
2025-09-17 12:42:09 +0530

    single product page based on All

--- src/app/(home)/products/[productId]/page.tsx
+++ src/app/(home)/products/[productId]/page.tsx
@@ -1,24 +1,54 @@
 "use client";
 
+import Loading from "@/app/loading";
 import Breadcrumbs from "@/components/Breadcrumbs";
 import RelatedProducts from "@/components/products/RelatedProducts";
 import SectionProduct from "@/components/products/SectionProductHeader";
 import ButtonLink from "@/shared/Button/ButtonLink";
-import { useStore } from "@/store/store-context";
+import { Product } from "@/store/product-store";
 import { makeUrlFromCategoryName } from "@/utils/url-generater";
+import { useQuery } from "@apollo/client/react";
+import gql from "graphql-tag";
 import { useParams } from "next/navigation";
 import { pathOr } from "ramda";
 import { useTranslation } from "react-i18next";
 
+const GET_PRODUCTS = gql`
+  query GetProducts {
+    products {
+      id
+      sku
+      name
+      description
+      features
+      category
+      subCategory
+      price
+      image
+      images
+      soh
+      moq
+      tag
+    }
+  }
+`;
+
+type GetProductsResponse = {
+  products: Product[];
+};
+
 const ProductPage = () => {
   const { t } = useTranslation();
   const params = useParams();
-  const { productStore } = useStore();
+  const { loading, error, data } = useQuery<GetProductsResponse>(GET_PRODUCTS);
 
   const productId = params?.["productId"];
-  const product = productStore.skuSearchList.find(
-    (item) => item.id.toString() === productId,
+  const product = data?.products.find(
+    (item) => item.id.toString() === productId
   );
+
+  if (loading) return <Loading />;
+  if (error) return <p>Error 😢 {error.message}</p>;
 
   if (!product) {
     return (
